951072bf63e3cf09a4b90aef0fe577889d3b4afd
peter <01peterkinyanjui@gmail.com>
2023-08-24 13:50:36 -0700

    newnode

--- module_1.c
+++ module_1.c
@@ -4,6 +4,7 @@
  * @stack: double pointer to the head of the stack
  * @line_number: the current line number
  */
+int number;
 
 void top_pint(stack_t **stack, unsigned int line_number)
 {
@@ -15,4 +16,27 @@ void top_pint(stack_t **stack, unsigned int line_number)
 	}
 	else
 		fprintf(stdout, "%d\n", (*stack)->n);
+}
+
+/**
+ * createnew_node - creates a new node
+ * @n: the value of the node
+ * Return: new node
+ */
+
+stack_t *createnewnode(int n)
+{
+	stack_t *new = NULL;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	return (new);
 }
