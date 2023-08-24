a295dba79d369ab347f330fedba39b1f073573c0
peter <01peterkinyanjui@gmail.com>
2023-08-24 14:12:29 -0700

    pop

--- module_1.c
+++ module_1.c
@@ -78,3 +78,28 @@ void pall_monty(stack_t **stack, unsigned int line_number)
 		tmp = tmp->next;
 	}
 }
+/**
+ * pop_monty - removes top mostelement of the stack.
+ * @stack: points to head of the stack
+ * @line_number: the current line number
+ */
+void pop_monty(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	else
+	{
+		temp = (*stack)->next;
+		free(*stack);
+		if (temp)
+			temp->prev = NULL;
+		*stack = temp;
+	}
+}
