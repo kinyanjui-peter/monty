57906c094ef6d91e73f8bda7ec726459a2ca3d6b
peter <01peterkinyanjui@gmail.com>
2023-08-25 04:52:32 -0700

    task

--- module_3.c
+++ module_3.c
@@ -88,3 +88,27 @@ void mod(stack_t **stack, unsigned int line_number)
 	pop(stack, line_number);
 	second_top->n = remainder;
 }
+/**
+ * _isdigit - checks if it is a digit
+ * Return: 0 or 1
+ * @c: Value
+ **/
+
+int _isdigit(char *c)
+{
+	char *temp = c;
+
+	if (c == NULL)
+		return (0);
+	if (*temp == '-')
+		temp++;
+
+	for (; *temp != '\0'; temp++)
+	{
+		if ((*temp < '0' || *temp > '9'))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
