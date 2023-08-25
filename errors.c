fb146afb10dd4ffeb401dc7052857b457792588e
peter <01peterkinyanjui@gmail.com>
2023-08-24 19:24:16 -0700

    task

--- errors.c
+++ errors.c
@@ -10,3 +10,12 @@ void usage_error(void)
 	exit(EXIT_FAILURE);
 
 }
+
+void instruction_error(FILE *fd, char *line, stack_t *stack, char *val, int line_n)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_n, val);
+	fclose(fd);
+	free(line);
+	freestack(stack);
+	exit(EXIT_FAILURE);
+}
