77358cb773002a7055ac209226b844a351803638
peter <01peterkinyanjui@gmail.com>
2023-08-24 19:44:54 -0700

    task

--- module_1.c
+++ module_1.c
@@ -49,53 +49,24 @@ stack_t *createnew_node(int n)
 
 void push_monty(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp, *new;
-	int i;
+	/**
+ * push - adds a new node with the given value to the stack
+ * @stk: double pointer to the head of the stack
+ * @line_num: value to be pushed
+ */
+void push(stack_t **stk, unsigned int line_num)
+{
+    stack_t *new_node = NULL;
+    (void)line_num;
 
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
-	{
-		set_tok_error(malloc_error());
-		return;
-	}
+    new_node = createnew_node(value);
 
-	if (op_toks[1] == NULL)
-	{
-		set_tok_error(no_int_error(line_number));
-		return;
-	}
-
-	for (i = 0; op_toks[1][i]; i++)
-	{
-		if (op_toks[1][i] == '-' && i == 0)
-			continue;
-		if (op_toks[1][i] < '0' || op_toks[1][i] > '9')
-		{
-			set_tok_error(no_int_error(line_number));
-			return;
-		}
-	}
-	new->n = atoi(op_toks[1]);
-
-	if (check_mode(*stack) == STACK) /* STACK mode insert at front */
-	{
-		tmp = (*stack)->next;
-		new->prev = *stack;
-		new->next = tmp;
-		if (tmp)
-			tmp->prev = new;
-		(*stack)->next = new;
-	}
-	else /* QUEUE mode insert at end */
-	{
-		tmp = *stack;
-		while (tmp->next)
-			tmp = tmp->next;
-		new->prev = tmp;
-		new->next = NULL;
-		tmp->next = new;
-	}
+    new_node->next = *stk;
+    if (*stk != NULL)
+        (*stk)->prev = new_node;
+    *stk = new_node;
 }
+
 
 
 /**
