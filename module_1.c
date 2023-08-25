1f6c07ad81b9c6eaef1bebd7d0478ccd882971d9
peter <01peterkinyanjui@gmail.com>
2023-08-25 03:58:29 -0700

    task

--- module_1.c
+++ module_1.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-int main_value;
+int value;
 
 /**
  * createNode - creates a new node
@@ -23,5 +23,52 @@ stack_t *createNode(int n)
     new_node->prev = NULL;
 
     return (new_node);
+}
+
+/** push - pushes a new node with the given value to the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number where the operation occurs (not used in this case)
+ * @value: value to be pushed onto the stack
+ */
+
+void push(stack_t **stack, unsigned int line_number, int value)
+{
+    stack_t *new = NULL;
+    (void)line_number;
+
+    new = createNode(value);
+
+    new->next = *stack;
+    if (*stack != NULL)
+        (*stack)->prev = new;
+    *stack = new;
+}
+
+#include "monty.h"
+
+/**
+ * pop - removes the top element of the stack.
+ * @stack: double pointer to the head of the stack
+ * @line_number: the current line number
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_top;
+
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	else
+	{
+		new_top = (*stack)->next;
+		free(*stack);
+		if (new_top)
+			new_top->prev = NULL;
+		*stack = new_top;
+	}
 }
 
