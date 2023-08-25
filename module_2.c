1badddb16024158000ce41cf01a4db931c1831bb
peter <01peterkinyanjui@gmail.com>
2023-08-24 18:10:29 -0700

    task

--- module_2.c
+++ module_2.c
@@ -88,3 +88,13 @@ void swap(stack_t **stack, unsigned int line_number)
         val->next->n = tmp;
     }
 }
+/**
+ * nop - function
+ * @stack: double pointer to the head of the list
+ * @line_number: the current line number
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
